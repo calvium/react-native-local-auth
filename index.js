2e1df255c9c47d4da64be983c55b8d093426d650
briancalvium <brian@calvium.com>
2016-08-26 10:46:37 +0100

    fix RN 30 release build issue - RN can choose .ios or .android

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import {
   Platform
 } from 'react-native'
 
-module.exports = Platform.OS === 'android' ? require('./LocalAuth.android') : require('./LocalAuth.ios')
+module.exports = require('./LocalAuth')
